7afd51a7f73560950fb3b579aaf0e77483ddeadd
Jason Bourne <ltrboks@gmail.com>
2020-11-03 10:39:39 +0200

    fix path separator for windows

--- fsw-server.js
+++ fsw-server.js
@@ -68,10 +68,11 @@ const handleAddChangeFile = async (event, path) => {
 };
 
 const handleChange = (event, path) => {
+console.log(`handleChange event=${event} path=${path}`)
   if(path.startsWith(baseDir)) {
       path = path.substring(baseDir.length);
   }
-  if(path[0]==="/") {
+  if(path[0]==="/" || path[0]==="\\") {
       console.log("trimming / from path");
       path = path.substring(1);
   }
